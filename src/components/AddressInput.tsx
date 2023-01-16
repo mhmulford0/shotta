d543a6a67823ea48e51fd703c6eb1bb7f73b9346
Matthew Mulford <mhmulford0@gmail.com>
2023-01-16 17:01:33 -0600

    cleanup, update text color

--- src/components/AddressInput.tsx
+++ src/components/AddressInput.tsx
@@ -1,4 +1,4 @@
-import { FormEvent, useReducer } from "react";
+import { useReducer } from "react";
 import { match } from "ts-pattern";
 
 type ApiResponse = {
@@ -26,7 +26,6 @@ async function checkTokenBalance(state: typeof initialState) {
   const data: ApiResponse = await res.json();
 
   console.log(data);
-  console.log(state);
 }
 
 function reducer(state: typeof initialState, action: Actions) {
@@ -52,7 +51,7 @@ export default function AddressInput() {
   return (
     <div className="flex min-h-full flex-col justify-center py-12 sm:px-6 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-md">
-        <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
+        <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-100">
           Get Token Balance
         </h2>
       </div>
@@ -82,7 +81,7 @@ export default function AddressInput() {
           </div>
 
           <div>
-            <label htmlFor="contractAddress" className="block font-medium text-gray-700">
+            <label htmlFor="contractAddress" className="block font-medium text-gray-700 mt-4">
               Contract Address
             </label>
             <div className="mt-1">
@@ -106,7 +105,7 @@ export default function AddressInput() {
           <div>
             <button
               type="submit"
-              className="flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+              className="flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 mt-2"
               onClick={() => checkTokenBalance(state)}
             >
               Search
