088177a0bc90475dea59a655c9fb4e80e11af528
Matthew Mulford <mhmulford0@gmail.com>
2023-01-16 17:10:17 -0600

    move abi

--- src/pages/api/tokenBalanceAtBlock.ts
+++ src/pages/api/tokenBalanceAtBlock.ts
@@ -7,6 +7,7 @@ export const config = {
 // http://localhost:3000/api/tokenBalanceAtBlock?contractAddress=0xb24cd494fae4c180a89975f1328eab2a7d5d8f11&wallet=0x75A6085Bbc25665B6891EA94475E6120897BA90b&blockNumber=123123
 
 import type { NextApiRequest, NextApiResponse } from "next";
+import { ERC20ABI } from "@/core/abis/ERC20";
 
 import { ethers } from "ethers";
 import { match } from "ts-pattern";
@@ -19,51 +20,6 @@ const reqInfo = z.object({
   contractAddress: z.string().length(42).startsWith("0x"),
   blockNumber: z.coerce.number().optional(),
 });
-
-const ERC20ABI = [
-  {
-    constant: false,
-    inputs: [
-      {
-        name: "_to",
-        type: "address",
-      },
-      {
-        name: "_value",
-        type: "uint256",
-      },
-    ],
-    name: "transfer",
-    outputs: [
-      {
-        name: "",
-        type: "bool",
-      },
-    ],
-    payable: false,
-    stateMutability: "nonpayable",
-    type: "function",
-  },
-  {
-    constant: true,
-    inputs: [
-      {
-        name: "_owner",
-        type: "address",
-      },
-    ],
-    name: "balanceOf",
-    outputs: [
-      {
-        name: "balance",
-        type: "uint256",
-      },
-    ],
-    payable: false,
-    stateMutability: "view",
-    type: "function",
-  },
-];
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   try {
